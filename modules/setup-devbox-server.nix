d4ccce6009572106cc8fb2e48427a6671fda0765
Archeri2000 <ongyuhann@hotmail.com>
2022-02-11 13:27:15 +0800

    Feat: setup-devbox-server

---
+++ modules/setup-devbox-server.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { } }:
+let setup-keys = import ./setup-keys.nix { inherit pkgs; }; in
+let set-signing-key = import ./set-signing-key.nix { inherit pkgs; }; in
+pkgs.writeShellScriptBin "setup-devbox-server" ''
+  #!/bin/sh
+
+  PATH=$PATH:${pkgs.coreutils}/bin
+
+  setup_keys=${setup-keys}/bin/setup-keys
+  set_signing_key=${set-signing-key}/bin/set-signing-key
+
+''
